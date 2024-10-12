20dc452c74c3a5c44e17feeba3edb45b871f5b27
Manojna2009 <manojnavinay09@gmail.com>
2024-10-12 15:16:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,9 +15,10 @@ document.addEventListener("DOMContentLoaded", function() {
         // Map the video parameter to a YouTube embed URL or any other video link
         let videoUrls = {
             video1: "https://tankie.tube/videos/embed/234b185f-c8cb-40f3-a48e-9072b53bad9f",
-            video2: "https://www.youtube.com/embed/9bZkp7q19f0",
-            video3: "https://www.youtube.com/embed/3JZ_D3ELwOQ",
-            video4: "https://www.youtube.com/embed/LjhWHffIx5E"
+            video2: "https://tankie.tube/videos/embed/8ec714bf-eaec-4ff6-93d3-13f495fccfc5",
+            video3: "https://tankie.tube/videos/embed/8bd6d7f3-cac4-4554-a306-687f65eedb67",
+            video4: "https://tankie.tube/videos/embed/097744e8-e5ec-45c0-8092-4c8e0af3867a",
+            video5: "https://tankie.tube/videos/embed/18d7ca10-f19a-48b8-bb47-8ce39b098e0e"
         };
 
         // If the videoParam exists and has a corresponding URL, load it
